f49f88209b8cf0c38720f0fac8e81bcceb986246
Rory Bunker <rorybunker@gmail.com>
2022-09-09 15:19:09 +0900

    Update training_test_splits_elo_welo.R

--- training_test_splits_elo_welo.R
+++ training_test_splits_elo_welo.R
@@ -3,6 +3,8 @@ list.of.packages <- c("welo")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
+library(welo)
+
 # load data
 load("atp_2005_2020.RData") 
 
